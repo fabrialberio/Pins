5f2dd1ed6b5d6decab50047a4245e87079e4a084
fabrialberio <->
2025-08-27 20:04:16 +0200

    refactor: Add `app_iterator` as a property of `PinsWindow`, allows showing app view after duplicating an app

--- src/pins-window.c
+++ src/pins-window.c
@@ -29,6 +29,8 @@ struct _PinsWindow
 {
     AdwApplicationWindow parent_instance;
 
+    PinsAppIterator *app_iterator;
+
     AdwNavigationView *navigation_view;
     PinsAppView *app_view;
     PinsFileView *file_view;
@@ -80,7 +82,11 @@ pins_window_save_current_desktop_file (PinsWindow *self)
 static void
 pins_window_dispose (GObject *object)
 {
+    PinsWindow *self = PINS_WINDOW (object);
+
     gtk_widget_dispose_template (GTK_WIDGET (object), PINS_TYPE_WINDOW);
+
+    g_object_unref (self->app_iterator);
 
     G_OBJECT_CLASS (pins_window_parent_class)->dispose (object);
 }
@@ -202,16 +208,16 @@ pins_window_add_new_app_cb (GSimpleAction *action, GVariant *param,
 
 void
 pins_window_duplicate_app_cb (GSimpleAction *action, GVariant *param,
-                              PinsAppIterator *app_iterator)
+                              PinsWindow *self)
 {
     const gchar *desktop_id = g_variant_get_string (param, NULL);
     GError *err = NULL;
 
-    g_assert (PINS_IS_APP_ITERATOR (app_iterator));
-
-    pins_app_iterator_duplicate_file (app_iterator, desktop_id, &err);
+    pins_app_iterator_duplicate_file (self->app_iterator, desktop_id, &err);
     if (err != NULL)
         g_warning ("Error duplicating file: %s", err->message);
+
+    adw_navigation_view_pop (self->navigation_view);
 }
 
 void
@@ -223,7 +229,6 @@ pins_window_file_activated_cb (PinsWindow *self, PinsDesktopFile *desktop_file)
 static void
 pins_window_init (PinsWindow *self)
 {
-    PinsAppIterator *app_iterator;
     g_autoptr (GSimpleAction) new_app_action = NULL,
                               duplicate_app_action = NULL,
                               search_action = NULL;
@@ -232,12 +237,12 @@ pins_window_init (PinsWindow *self)
 
     pins_inject_icon_search_paths ();
 
-    app_iterator = pins_app_iterator_new ();
+    self->app_iterator = pins_app_iterator_new ();
 
     new_app_action = g_simple_action_new ("new-app", NULL);
     g_signal_connect_object (new_app_action, "activate",
                              G_CALLBACK (pins_window_add_new_app_cb),
-                             app_iterator, 0);
+                             self->app_iterator, 0);
     g_action_map_add_action (G_ACTION_MAP (self), G_ACTION (new_app_action));
 
     duplicate_app_action
@@ -252,7 +257,7 @@ pins_window_init (PinsWindow *self)
         "search", NULL, g_variant_new_boolean (FALSE));
     g_action_map_add_action (G_ACTION_MAP (self), G_ACTION (search_action));
 
-    pins_app_view_set_app_iterator (self->app_view, app_iterator);
+    pins_app_view_set_app_iterator (self->app_view, self->app_iterator);
 
     g_signal_connect_object (self->app_view, "activate",
                              G_CALLBACK (pins_window_file_activated_cb), self,
