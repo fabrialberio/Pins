c82c3167bea1345a09fc1eb5c99acbe625f67654
fabrialberio <->
2026-01-05 15:46:58 +0100

    feat: Add categories to PinsAppFilter

--- src/pins-app-filter.h
+++ src/pins-app-filter.h
@@ -26,6 +26,15 @@ G_BEGIN_DECLS
 
 #define PINS_TYPE_APP_FILTER (pins_app_filter_get_type ())
 
+typedef enum
+{
+    PINS_APP_FILTER_CATEGORY_ALL,
+    PINS_APP_FILTER_CATEGORY_VISIBLE, // System or edited, aka not hidden.
+    PINS_APP_FILTER_CATEGORY_EDITED,
+    PINS_APP_FILTER_CATEGORY_SYSTEM,
+    PINS_APP_FILTER_CATEGORY_HIDDEN,
+} PinsAppFilterCategory;
+
 G_DECLARE_FINAL_TYPE (PinsAppFilter, pins_app_filter, PINS, APP_FILTER,
                       GObject);
 
@@ -33,5 +42,6 @@ PinsAppFilter *pins_app_filter_new (void);
 
 void pins_app_filter_set_model (PinsAppFilter *self, GListModel *model);
 void pins_app_filter_set_search (PinsAppFilter *self, const gchar *query);
+void pins_app_filter_reset_category (PinsAppFilter *self);
 
 G_END_DECLS
